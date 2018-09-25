8602b51c2f3037c16e8b1f39e2f09232af6389a1
DarthPigrum <43322390+DarthPigrum@users.noreply.github.com>
2018-09-25 21:26:22 +0300

    Updated examples

--- kdm/index.js
+++ kdm/index.js
@@ -17,5 +17,22 @@ let C = [[0,0,0,0,0,0,0],
          [0,0,0,1,1,1,0],
          [0,0,0,1,1,1,0],
          [0,0,0,0,0,0,0]]
-console.log(kdm.findSampleAB(kdm.Xor(kdm.Xor(kdm.And(A,kdm.Not(B)),kdm.And(kdm.Not(A),B)),kdm.And(kdm.Not(A),kdm.Not(B))),A,B))
-console.log(kdm.findSampleAB(kdm.Not(kdm.Xor(kdm.Not(kdm.Xor(A,B)),kdm.Not(A))),A,B))
+//example
+let task1 = kdm.Not(kdm.And(kdm.Not(kdm.And(kdm.Not(kdm.And(kdm.Not(kdm.And(A,C)),B)),kdm.Not(B))),A))
+console.log("Task 1:")
+console.log(kdm.findSampleAB(task1,A,B))
+let task2 = kdm.Not(kdm.Or(kdm.Not(kdm.And(kdm.Not(kdm.Or(A,B)),kdm.Not(B))),kdm.Not(kdm.And(A,B))))
+console.log("Task 2:")
+kdm.matrixPrinter(task2)
+//example in classes
+console.log("In classes:")
+let a = new kdm.Matrix(A)
+let b = new kdm.Matrix(B)
+let c = new kdm.Matrix(C)
+let not = kdm.not
+console.log("Task 1:")
+let task1class = not(not(not(not(a.and(c)).and(b)).and(not(b))).and(a))
+console.log(kdm.findSampleAB(task1class.arr,a.arr,b.arr))
+console.log("Task 1:")
+let task2class = not(not(not(a.or(b)).and(not(b))).or(not(b.and(a))))
+kdm.matrixPrinter(task2class.arr)
