e3f3b565d1f36d3a64d1ddf7908777c0959e2f33
DarthPigrum <43322390+DarthPigrum@users.noreply.github.com>
2018-09-25 20:39:57 +0300

    Changed exports
    No function rewriting for export

--- kdm/kdm-lib.js
+++ kdm/kdm-lib.js
@@ -105,111 +105,11 @@ function findSampleAB(matrix,A,B){
     if(matrixCompare(matrix,Not(Dif(B,A)))){return "!(B/A)"}
     return false
 }
-module.exports = {
-matrixPrinter: function(matrix){
-    let i = 0
-    while(matrix.length>i){
-        console.log(matrix[i])
-        i++
-    }
-},
-matrixCompare: function(matrix1,matrix2){
-    if(JSON.stringify(matrix1) == JSON.stringify(matrix2)){
-        return true
-    }else{
-        return false
-    }
-},
-Not: function(matrix){
-    let i = 0
-    let matrixR = JSON.parse(JSON.stringify(matrix))
-    while(matrix.length>i){
-        let j = 0
-        while(matrix[i].length>j){
-            matrixR[i][j] = (matrix[i][j]-1)*(matrix[i][j]-1)
-            j++
-        }
-        i++
-    }
-    return(matrixR)
-},
-And: function(matrix1,matrix2){
-    let i = 0
-    let matrixR = JSON.parse(JSON.stringify(matrix1))
-    while(matrix1.length>i){
-        let j = 0
-        while(matrix1[i].length>j){
-            matrixR[i][j]=matrix1[i][j]*matrix2[i][j]
-            j++
-        }
-        i++
-    }
-    return(matrixR)
-},
-Or: function(matrix1,matrix2){
-    let i = 0
-    let matrixR = JSON.parse(JSON.stringify(matrix1))
-    while(matrix1.length>i){
-        let j = 0
-        while(matrix1[i].length>j){
-            if(matrix1[i][j] == 1 || matrix2[i][j] == 1){
-                matrixR[i][j] = 1
-            }else{
-                matrixR[i][j] = 0
-            }
-            j++
-        }
-        i++
-    }
-    return(matrixR)
-},
-Xor: function(matrix1,matrix2){
-    let i = 0
-    let matrixR = JSON.parse(JSON.stringify(matrix1))
-    while(matrix1.length>i){
-        let j = 0
-        while(matrix1[i].length>j){
-            if(matrix1[i][j] == 1 ^ matrix2[i][j] == 1){
-                matrixR[i][j] = 1
-            }else{
-                matrixR[i][j] = 0
-            }
-            j++
-        }
-        i++
-    }
-    return(matrixR)
-},
-Dif: function(matrix1,matrix2){
-    let i = 0
-    let matrixR = JSON.parse(JSON.stringify(matrix1))
-    while(matrix1.length>i){
-        let j = 0
-        while(matrix1[i].length>j){
-            if(matrix1[i][j] == 1 && matrix2[i][j] == 0){
-                matrixR[i][j] = 1
-            }else{
-                matrixR[i][j] = 0
-            }
-            j++
-        }
-        i++
-    }
-    return(matrixR)
-},
-findSampleAB: function(matrix,A,B){
-    if(matrixCompare(matrix,Not(A))){return "!A"}
-    if(matrixCompare(matrix,Not(B))){return "!B"}
-    if(matrixCompare(matrix,Or(A,B))){return "AuB"}
-    if(matrixCompare(matrix,Not(Or(A,B)))){return "!(AuB)"}
-    if(matrixCompare(matrix,And(A,B))){return "AnB"}
-    if(matrixCompare(matrix,Not(And(A,B)))){return "!(AnB)"}
-    if(matrixCompare(matrix,Xor(A,B))){return "A^B"}
-    if(matrixCompare(matrix,Not(Xor(A,B)))){return "!(A^B)"}
-    if(matrixCompare(matrix,Dif(A,B))){return "A/B"}
-    if(matrixCompare(matrix,Not(Dif(A,B)))){return "!(A/B)"}
-    if(matrixCompare(matrix,Dif(B,A))){return "B/A"}
-    if(matrixCompare(matrix,Not(Dif(B,A)))){return "!(B/A)"}
-    return false
-}
-}
+module.exports.matrixPrinter = matrixPrinter
+module.exports.matrixCompare = matrixCompare
+module.exports.Not = Not
+module.exports.And = And
+module.exports.Or = Or
+module.exports.Xor = Xor
+module.exports.Dif = Dif
+module.exports.findSampleAB = findSampleAB
