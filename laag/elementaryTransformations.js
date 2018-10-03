5d964fdf07479b5ef4a43f85d217d63bfe4b6c1b
DarthPigrum <43322390+DarthPigrum@users.noreply.github.com>
2018-10-03 21:49:31 +0300

    Added optimization by a-kuharenko

--- laag/elementaryTransformations.js
+++ laag/elementaryTransformations.js
@@ -23,19 +23,14 @@ input.on('data', function (data) {
     let op = "+"
     let mult = 1
     let adder = 0
-    for(let i = 0;i < data.length;i++){
-        if(data[i] === "+"){op = "+";break}
-        if(data[i] === "-"){op = "-";break}
-    }
+    if(data.includes("+")){op = "+"}
+    if(data.includes("-")){op = "-"}
     const sum = data.split(op)
-    for(let i = 0;i < data.length;i++){
-        if(data[i] === "*"){
+        if(data.includes("*")){
             const multiplication = sum[1].split("*")
             mult = parseFloat(multiplication[0])
             adder = parseInt(multiplication[1])
-            break;}
-        adder = parseInt(sum[1])
-    }
+        }else{adder = parseInt(sum[1])}
     const base = parseInt(sum[0])
     elemTransform(matrix,base,adder,mult,op)
     matrixPrinter(matrix)
